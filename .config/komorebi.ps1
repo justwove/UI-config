e54289f1e100181401efea3c1ffd388dc289ba98
Wove <53346616+justwove@users.noreply.github.com>
2025-04-04 20:24:05 +0000

    initial commit

---
+++ .config/komorebi.ps1
@@ -0,0 +1,20 @@
+komorebic stop --whkd
+komorebic start --whkd
+# komorebic focused-workspace-container-padding 1
+# komorebic focused-workspace-padding 0
+
+# Monitor 0
+komorebic container-padding 0 0 1
+komorebic workspace-padding 0 0 0
+komorebic container-padding 0 1 1
+komorebic workspace-padding 0 1 0
+komorebic container-padding 0 2 1
+komorebic workspace-padding 0 2 0
+
+# Monitor 1
+komorebic container-padding 1 0 1
+komorebic workspace-padding 1 0 0
+komorebic container-padding 1 1 1
+komorebic workspace-padding 1 1 0
+komorebic container-padding 1 2 1
+komorebic workspace-padding 1 2 0
